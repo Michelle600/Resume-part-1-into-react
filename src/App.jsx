9bc8bc1e965c3494edf18950c5d05187074dc763
Michelle600 <michellelim629@gmail.com>
2024-09-07 13:58:57 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <>
+      <h1>Resume</h1>
+      <h2>Michelle</h2>
+      <Experiance />
+    </>
+  );
+}
+
+function Experiance() {
+  return (
+    <>
+      <h3> Software Developer</h3>
+      <h4> Software Developer</h4>
+    </>
+  );
+}
